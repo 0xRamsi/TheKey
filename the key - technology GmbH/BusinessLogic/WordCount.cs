67b8681a7cb0c236910643de5909034ebbbcaa5d
Ramzi Khahil <ramzi.khahil@pm.me>
2023-03-02 16:35:19 +0100

    Seperated logic into it's own class.

---
+++ the key - technology GmbH/BusinessLogic/WordCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace the_key___technology_GmbH.BusinessLogic
+{
+    public class WordCount
+    {
+        public static IDictionary<string, int> GetWordCount(string content)
+        {
+            IDictionary<string, int> wordCount = new Dictionary<string, int>();
+            content = Regex.Replace(content, "<.*?>", string.Empty);        // Not sure this is a good way to strip HTML, but good enough for a coding homework.
+            content = Regex.Replace(content, "[\"',.;:()]", string.Empty);
+
+            string[] allWords = content.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (string word in allWords)
+            {
+                if (wordCount.Keys.Contains(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount.Add(word, 1);
+                }
+            }
+
+            return wordCount;
+        }
+    }
+}
--- the key - technology GmbH/Controllers/TheKeyAcademyController.cs
+++ the key - technology GmbH/Controllers/TheKeyAcademyController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using the_key___technology_GmbH.BusinessLogic;
 using the_key___technology_GmbH.Models;
 
 namespace the_key___technology_GmbH.Controllers
@@ -64,30 +65,10 @@ namespace the_key___technology_GmbH.Controllers
                         type = post.type,
                         categories = post.categories,
                         tags = post.tags,
-                        theMap = GetWordCountForBlogpost(post.content.rendered),
+                        theMap = WordCount.GetWordCount(post.content.rendered),
                     };
 
             return JsonConvert.SerializeObject(result);
-        }
-
-        private IDictionary<string, int> GetWordCountForBlogpost(string content)
-        {
-            IDictionary<string, int> wordCount = new Dictionary<string, int>();
-            content = Regex.Replace(content, "<.*?>", string.Empty);        // Not sure this is a good way to strip HTML, but good enough for a coding homework.
-            content = Regex.Replace(content, "[\"',.;:()]", string.Empty);
-
-            string[] allWords = content.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            foreach (string word in allWords)
-            {
-                if(wordCount.Keys.Contains(word))
-                {
-                    wordCount[word]++;
-                } else {
-                    wordCount.Add(word, 1);
-                }
-            }
-
-            return wordCount;
         }
     }
 }
